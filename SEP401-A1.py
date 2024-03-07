684637b1a4c7fca64e8d79e640301292148f051a
Barbosa <63185850+barbosa-wellington@users.noreply.github.com>
2024-03-07 09:42:14 -0300

    button using thiker
    This is a script in Python that implement button conception relates to  GUI.

---
+++ SEP401-A1.py
@@ -0,0 +1,56 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.geometry('350x300')
+root.resizable(width=False, height=False)
+
+colour1 = '#020f12'
+colour2 = '#05d7ff'
+colour3 = '#65e7ff'
+colour4 = 'BLACK'
+
+main_frame = tk.Frame(root, bg=colour1, pady=40)
+main_frame.pack(fill=tk.BOTH, expand=True)
+main_frame.columnconfigure(0,weight=1)
+main_frame.rowconfigure(0,weight=1)
+main_frame.rowconfigure(1,weight=1)
+
+button1 = tk.Button(
+    main_frame,
+    background=colour2,
+    foreground=colour4,
+    activebackground=colour3,
+    activeforeground=colour4,
+    highlightthickness=2,
+    highlightbackground=colour2,
+    highlightcolor='WHITE',
+    width=12,
+    height=2,
+    border=0,
+    cursor='hand1',
+    text='Login',
+    font=('Arial', 16, 'bold')
+
+)
+
+button2 = tk.Button(
+    main_frame,
+    background=colour1,
+    foreground=colour2,
+    activebackground=colour3,
+    activeforeground=colour4,
+    highlightthickness=2,
+    highlightbackground=colour2,
+    highlightcolor='WHITE',
+    width=12,
+    height=2,
+    border=0,
+    cursor='hand1',
+    text='Sign up',
+    font=('Arial', 16, 'bold')
+
+)
+button1.grid(column=0, row=0)
+button2.grid(column=0, row=1)
+root.mainloop()
+
