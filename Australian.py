d662a4b7b4fe4285274ff3fd0a58a94060d7f8db
Barbosa <63185850+barbosa-wellington@users.noreply.github.com>
2024-03-10 09:43:09 -0300

    Add files via upload

---
+++ Australian.py
@@ -0,0 +1,14 @@
+class Austraion():
+    is_human = True
+    enjoys_sport = True
+
+
+class Pet():
+    """
+    A class to capture useful information regarding my pets, just incase
+    I lose track of them.
+    
+    """
+
+    is_human = False
+    owner = 'Michael Smith'
