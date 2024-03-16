062cab7876a59904112093247c5a10c00e7bbdfd
Barbosa <63185850+barbosa-wellington@users.noreply.github.com>
2024-03-16 20:37:34 +1100

    simple script library

---
+++ wk_function.py
@@ -0,0 +1,24 @@
+"""
+    Working with assigned
+"""
+
+
+import datetime
+import time
+
+# You can call this function without assigned
+# This is a function with arguments
+def add_up(x, y):
+    return x + y
+
+
+"""
+    Different of exampel above, we can creat a scritps without arguments
+    below we can use to the past example of exercise.
+    Function without arguments
+"""
+
+def get_the_time():
+
+    sentence = 'Hello new world Python', time.strftime("%H:%M:%S")
+    return print(sentence)
