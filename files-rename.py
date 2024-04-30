18330cb67d6c141e1894c739e7fb3b62af5092be
Barbosa <63185850+barbosa-wellington@users.noreply.github.com>
2024-04-30 15:57:05 +1000

    Update

--- files-rename.py
+++ files-rename.py
@@ -1,11 +1,14 @@
 import os
 
 # warning: We need add the \\ at the end of path, otherwise the system will not find the files.
-path = r"C:\Users\User-Name\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\\"
+#path = r"C:\Users\User-Name\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\\"
+
+path = r"C:\Users\wellb\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\\"
 # I created a new folder just to make sure that I will not alter any important file on the windows main directory
 #path = r"C:\tmp\file-picture\\"
 dir_list = os.listdir(path)
 
+print(dir_list)
 n_list = []
 n_conv = []
 test_list = []
@@ -19,8 +22,13 @@ for i in range(dir_list.__len__()):
 for i, j in zip(dir_list, n_conv):
     n_list.append(i+j)
 
-# Use the os function rename to alter the extension of the files to jpg 'picture'.
-# Crate a for which use the zip method which allow us to run 2 list at the same time and pass the velue to new variables.
-
-for i, j in zip(dir_list, n_list):
+print(n_list)
+# # Use the os function rename to alter the extension of the files to jpg 'picture'.
+# # Crate a for which use the zip method which allow us to run 2 list at the same time and pass the velue to new variables.
+#
+# renaming all the file on this folder by using the old name and convert into the list n_conv which has new labels '01wall.jpg'
+# the path was fixed since it did not contain the \
+for i, j in zip(dir_list, n_conv):
+    # print(path+j)
     os.rename(path+i, path+j)
+
